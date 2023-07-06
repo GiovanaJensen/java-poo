274f9dc7562e1918f2ecf09bf0f2f3a0fc6d66a2
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-07-06 14:42:13 -0300

    organizando os imports e packages

--- src/br/com/bytebank/banco/testes/App.java
+++ src/br/com/bytebank/banco/testes/App.java
@@ -1,3 +1,9 @@
+package br.com.bytebank.banco.testes;
+import br.com.bytebank.banco.modelo.Cliente;
+import br.com.bytebank.banco.modelo.Conta;
+import br.com.bytebank.banco.modelo.ContaCorrente;
+import br.com.bytebank.banco.modelo.SaldoInsuficienteException;
+
 public class App {
     public static void main(String[] arg) throws SaldoInsuficienteException{
         Conta primeiraConta = new ContaCorrente(100,200);
