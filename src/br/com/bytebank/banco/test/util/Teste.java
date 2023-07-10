476fb3996c515034d5a857186eba5d02d7c6eb72
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-07-09 21:37:03 -0300

    tipificando a array

--- src/br/com/bytebank/banco/test/util/Teste.java
+++ src/br/com/bytebank/banco/test/util/Teste.java
@@ -3,10 +3,13 @@ package br.com.bytebank.banco.test.util;
 import java.util.ArrayList;
 import br.com.bytebank.banco.modelo.ContaCorrente;
 import br.com.bytebank.banco.modelo.ContaPoupanca;
+import br.com.bytebank.banco.modelo.Conta;
 
 public class Teste {
     public static void main(String[] args){
-        ArrayList lista = new ArrayList();
+
+        //Generics
+        ArrayList<Conta> lista = new ArrayList<Conta>();
         ContaCorrente cc = new ContaCorrente(11,22);
         ContaPoupanca cp = new ContaPoupanca(22,33);
 
@@ -14,8 +17,8 @@ public class Teste {
         lista.add(cp);
         System.out.println(lista.size());
 
-        for(Object objectReferencia : lista){
-            System.out.println(objectReferencia);
+        for(Conta conta : lista){
+            System.out.println(conta);
         }
     }
     
