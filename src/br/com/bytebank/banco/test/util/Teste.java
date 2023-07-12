f2eda44bdfdb2dd65e96a7ae1d091f0057262f49
GiovanaJensen <oliveirasgiovana143@gmail.com>
2023-07-11 21:29:20 -0300

    testando lista de inteiros usando o pacote java.util

---
+++ src/br/com/bytebank/banco/test/util/Teste.java
@@ -0,0 +1,11 @@
+package br.com.bytebank.banco.test.util;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Teste {
+    public static void main(String[] args){
+        List<Integer> numeros = new ArrayList<Integer>();
+        numeros.add(29);
+    }
+}
